8a0d6305cd650206be6add4dfa561b11c6c1a8e0
Benjamin <Benjamin@9a2719d2-1c2b-0a45-af5b-2640030c331b>
2016-08-11 15:14:49 +0000

    delete commit
    git-svn-id: file:///E/Local_Repository/Asset_Health/trunk@5 9a2719d2-1c2b-0a45-af5b-2640030c331b

--- Assets/scripts/SystemConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SystemConfig {
-
-    static public bool IsDebugOn { get {return true; }  }
-}
--- Assets/scripts/Version.cs
+++
@@ -1,6 +0,0 @@
-public class Version
-{
-	public const int revision = 11200;
-	public const string bundleVersion = "1.0.0";
-	public const int bundleBuildNo = 5;
-}
