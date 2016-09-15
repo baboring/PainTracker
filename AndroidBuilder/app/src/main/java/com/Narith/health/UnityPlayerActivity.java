9afd4dbe1f05aa3c06bea6a6c483464e2b427d6c
Benjamin <Benjamin@9a2719d2-1c2b-0a45-af5b-2640030c331b>
2016-09-15 03:52:37 +0000

    [일차 수정 저장용]
    git-svn-id: file:///E/Local_Repository/Asset_Health/trunk@16 9a2719d2-1c2b-0a45-af5b-2640030c331b

---
+++ AndroidBuilder/app/src/main/java/com/Narith/health/UnityPlayerActivity.java
@@ -0,0 +1,8 @@
+package com.narith.health;
+
+import com.unity3d.player.*;
+
+/**
+ * @deprecated Use UnityPlayerNativeActivity instead.
+ */
+public class UnityPlayerActivity extends UnityPlayerNativeActivity { }
